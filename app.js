c14d4eeb3894c1d10aa8ffdd3861be351a97da6b
RameenSurryya <rameensurryya@gmail.com>
2026-05-09 18:36:04 +0500

    feat: connect mongodb database

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const path = require("path");
 require("dotenv").config();
+const connectDB = require("./config/db");
+
+connectDB();
 
 const app = express();
 
